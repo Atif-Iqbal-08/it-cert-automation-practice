56b5ddaaaba922eef34e9bea0954bed267bbb576
Atif-Iqbal-08 <atifiqbal08@gmail.com>
2022-08-30 17:25:51 +0000

    Closes: #1 Updated validations.py python script. Fixed the behavior of validate_user function in validations.py.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -21,6 +21,8 @@ def validate_user(username, minlen):
     if not re.match("[A-Za-z0-9]",username[0]):
         return False
     return True
+
+#adding for missing comment
 print(validate_user("blue.kale", 3)) # True
 print(validate_user(".blue.kale", 3)) # Currently True, should be False
 print(validate_user("red_quinoa", 4)) # True
